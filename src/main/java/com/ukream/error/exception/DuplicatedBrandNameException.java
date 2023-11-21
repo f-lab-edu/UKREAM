f1520d3914c9b03d08909a962b19b3e8591bc96a
park-yeong-ung <youngung2213@gmail.com>
2023-11-21 18:22:25 +0900

    #10 review default메세지 예외처리

--- src/main/java/com/ukream/error/exception/DuplicatedBrandNameException.java
+++ src/main/java/com/ukream/error/exception/DuplicatedBrandNameException.java
@@ -1,6 +1,9 @@
 package com.ukream.error.exception;
 
 public class DuplicatedBrandNameException extends RuntimeException {
+  public DuplicatedBrandNameException(){
+    super("중복된 브랜드명 입니다.");
+  }
   public DuplicatedBrandNameException(String msg) {
     super(msg);
   }
